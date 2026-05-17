a28e303955ba8ee0d754c8b4644aab2cad5586ed
Hamza Awad <hamzaawad8899@gmail.com>
2026-05-18 01:04:42 +0300

    Initial commit

---
+++ src/main/java/com/batatis/BatatisDataGenerator.java
@@ -0,0 +1,11 @@
+package com.batatis;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BatatisDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
